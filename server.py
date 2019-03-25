c83b3d786d1b46317ef3dbae6d8a5e5e150cf316
Ankit Gulekar <ankitgulekar@Ankits-MacBook-Pro.local>
2019-03-24 19:07:31 -0700

    changes

--- server.py
+++ server.py
@@ -1,17 +1,22 @@
 from socket import *
+from threading import Thread
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print('The server is ready to receive')
-while True:
+#while True:
+def clientmanager():
     connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024).decode()
+    while True:
+        sentence = connectionSocket.recv ( 1024 ).decode ()
     if(sentence == "Client X: Alice"):
         Sentence = "X: Alice received before Y: Bob"
     else:
         Sentence = "Y: Bob received before X: Alice"
     connectionSocket.send(Sentence.encode())
-    connectionSocket.close()
 
+for i in range(2):
+    Thread(target=clientmanager).start()
 
+serverSocket.close()

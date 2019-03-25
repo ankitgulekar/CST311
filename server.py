9ddbbe894dbe2b7e251767ade249647c84070520
Erick Shaffer <erickshaffer@Ericks-MacBook-Pro.local>
2019-03-24 20:32:12 -0700

    fixed error

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from socket import *
-from threading import Thread
+import thread
 
 BUFF = 1024
 HOST = '127.0.0.1'  # must be input parameter @TODO
@@ -12,31 +12,25 @@ def response(key):
 
 def handler(clientsock, addr):
     while 1:
-        data = clientsock.recv ( BUFF )
+        data = clientsock.recv(BUFF)
         if not data: break
-        print
-        repr ( addr ) + ' recv:' + repr ( data )
-        clientsock.send ( response ( data ) )
-        print
-        repr ( addr ) + ' sent:' + repr ( response ( data ) )
-        if "close" == data.rstrip (): break  # type 'close' on client console to close connection from the server side
-    
-    clientsock.close ()
-    print
-    addr, "- closed connection"  # log on console
+        print repr(addr) + ' recv:' + repr(data)
+        clientsock.send(response(data))
+        print repr(addr) + ' sent:' + repr(response(data))
+        if "close" == data.rstrip(): break  # type 'close' on client console to close connection from the server side
+
+    clientsock.close()
+    print addr, "- closed connection"  # log on console
 
 
 if __name__ == '__main__':
     ADDR = (HOST, PORT)
-    serversock = socket ( AF_INET, SOCK_STREAM )
-    serversock.setsockopt ( SOL_SOCKET, SO_REUSEADDR, 1 )
-    serversock.bind ( ADDR )
-    serversock.listen ( 5 )
+    serversock = socket(AF_INET, SOCK_STREAM)
+    serversock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    serversock.bind(ADDR)
+    serversock.listen(5)
     while 1:
-        print
-        'waiting for connection... listening on port', PORT
-        clientsock, addr = serversock.accept ()
-        print
-        '...connected from:', addr
-        thread.start_new_thread ( handler, (clientsock, addr) )
-
+        print 'waiting for connection... listening on port', PORT
+        clientsock, addr = serversock.accept()
+        print '...connected from:', addr
+        thread.start_new_thread(handler, (clientsock, addr))

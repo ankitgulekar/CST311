7286fc9117be170713cfa6ab20c8b4d59eeb6436
ankitgulekar <42821708+ankitgulekar@users.noreply.github.com>
2019-03-24 17:32:57 -0700

    Update ClientX.py

--- ClientX.py
+++ ClientX.py
@@ -4,18 +4,20 @@ serverName = localhost
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
-
+message = 'Client X: Alice'
 for i in range(5):
-  Thread(target=clientHandler).start()
+    Thread(target=clienthandler).start()
 
 
-def clientHandler():
+def clienthandler():
     connectionSocket, addr = clientSocket.accept()
-    print (addr, "Client X: Alice")
+    #print (addr, "Client X: Alice")
     while 1:
         message = connectionSocket.recv(1024)
         if not message:
-            break      
- clientSocket.close()
+            break
+
+
+clientSocket.close()
 
 

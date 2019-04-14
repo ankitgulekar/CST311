94f3bcc57eaf31f6dd59d0d45873dd77a5602ccc
Erick Shaffer <erickshaffer@Ericks-MacBook-Pro.local>
2019-04-14 13:22:47 -0700

    working h1 ping h2

--- PA3/PA3.py
+++ PA3/PA3.py
@@ -7,37 +7,37 @@ from mininet.log import setLogLevel, info
 
 
 def myNetwork():
-    
-    net = Mininet ( topo=None,
-                    build=False,
-                   )
+    net = Mininet(topo=None,
+                  build=False,
+                  ipBase='192.168.1.1/24'
+                  )
 
-    default_ip = '10.0.0.1/8'
-    info ( '*** Add Router\n' )
-    r1 = net.addHost ( 'r1', cls=Node, ip=default_ip)
-    r1.cmd ( 'sysctl -w net.ipv4.ip_forward=1' )
+    default_ip = '192.168.1.1/24'
+    info('*** Add Router\n')
+    r1 = net.addHost('r1', cls=Node, ip=default_ip)
+    r1.cmd('sysctl -w net.ipv4.ip_forward=1')
 
-    info ( '*** Add Switch\n' )
+    info('*** Add Switch\n')
     switch = net.addSwitch('s1')
-    net.addLink (switch, r1 )
+    # net.addLink (switch, r1 )
 
-    info ( '*** Add hosts\n' )
-    h1 = net.addHost ( 'h1', cls=Host, ip='10.0.0.2/8')
-    h2 = net.addHost ( 'h2', cls=Host, ip='10.0.0.3/8')
+    info('*** Add hosts\n')
+    h1 = net.addHost('h1', cls=Host, ip='192.168.1.100/24', defaultRoute='via 192.168.1.1')
+    h2 = net.addHost('h2', cls=Host, ip='172.16.0.100/12', defaultRoute='via 172.16.0.1')
 
-    info ( '*** Add links\n' )
-    net.addLink ( h1, switch )
-    net.addLink ( h2, switch )
+    info('*** Add links\n')
+    net.addLink(h1, r1, intfName2='r0-eth1', params2={'ip': '192.168.1.1/24'})
+    net.addLink(h2, r1, intfName2='r0-eth2', params2={'ip': '172.16.0.1/12'})
 
-    info ( '*** Starting network\n' )
-    net.build ()
-    #net.start()
-    info ( '*** Routing Table on Router:\n' )
-    info ( net['r1'].cmd ( 'route' ) )
-    CLI ( net )
-    net.stop ()
+    info('*** Starting network\n')
+    net.build()
+
+    info('*** Routing Table on Router:\n')
+    info(net['r1'].cmd('route'))
+    CLI(net)
+    net.stop()
 
 
 if __name__ == '__main__':
-    setLogLevel ( 'info' )
-    myNetwork ()
+    setLogLevel('info')
+    myNetwork()
